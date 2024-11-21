1e354acb32e99ba5470ff0e98bad363720c668b4
zk18l <zaidalpero63@gmail.com>
2024-11-21 12:47:16 +0300

    edit available and make the program calculate it add two array

--- cpu.c
+++ cpu.c
@@ -1,99 +1,132 @@
+// Bankers Algorithm
+
 #include <stdio.h>
-#include <limits.h>
-void FCFS() {
-    // Code for FCFS scheduling
-}
 
-void SJF() {
-   int nprocess;
-    int avgwt = 0, temp;
+void main()
+{
+    int process, resource, i,instance, j, k = 0, count = 0, temp = 0;
+    printf("\n Enter Number of Process : ");
+    scanf("%d", &process);
+    int system[process];
+    printf(" Enter Number of Resources : ");
+    scanf("%d", &resource);
+    int available[resource],max_instance[resource], max[process][resource], allocated[process][resource], need[process][resource], completed[process], total[resource];
 
-    // Input the number of processes
-    printf("\nEnter the number of processes: ");
-    scanf("%d", &nprocess);
+    for (i = 0; i < process; i++)
+        completed[i] = 0;
+    for (j = 0; j < resource; j++)
+        total[j] = 0;
 
-    int burstTime[nprocess];
-    int waitingTime[nprocess];
-    int turnaroundTime[nprocess];
-
-    // Input the burst time for each process
-    printf("\nEnter the burst time for each process:\n");
-    for (int i = 0; i < nprocess; i++) {
-        printf("Burst time for P%d: ", i + 1);
-        scanf("%d", &burstTime[i]);
+    printf("\n Enter No. of Max Instances for each resource\n\n");
+    for (i = 0; i < resource; i++)
+    {
+        printf(" Resources[%d] : ", i);
+        scanf("%d", &max_instance[i]);
+        
     }
 
-    // Sort the processes based on burst time (SJF)
-    for (int i = 0; i < nprocess - 1; i++) {
-        for (int j = i + 1; j < nprocess; j++) {
-            if (burstTime[i] > burstTime[j]) {
-                // Swap burst times
-                temp = burstTime[i];
-                burstTime[i] = burstTime[j];
-                burstTime[j] = temp;
-            }
+    printf("\n------------------------------------------------------------------------------\n");
+    printf("\n Enter MAXIMUM instance for a Process & its corresponding resource :\n");
+    for (i = 0; i < process; i++)
+    {
+        printf("\n For Process[%d] \n", i);
+        for (j = 0; j < resource; j++)
+        {
+            printf(" Resource[%d] : ", j);
+            scanf("%d", &instance);
+            max[i][j] = instance;
         }
     }
 
-    // Calculate waiting time for each process
-    waitingTime[0] = 0; // First process has zero waiting time
-    for (int i = 1; i < nprocess; i++) {
-        waitingTime[i] = waitingTime[i - 1] + burstTime[i - 1];
+    printf("\n------------------------------------------------------------------------------\n");
+    printf("\n Enter instance ALLOCATED for a Process & its corresponding resource :\n");
+    for (i = 0; i < process; i++)
+    {
+        printf("\n For Process[%d] \n", i);
+        for (j = 0; j < resource; j++)
+        {
+            printf(" Resource[%d] : ", j);
+            scanf("%d", &instance);
+            allocated[i][j] = instance;
+            need[i][j] = max[i][j] - allocated[i][j];
+        }
     }
 
-   
-    // Calculate total waiting time and total turnaround time
-    for (int i = 0; i < nprocess; i++) {
-        avgwt += waitingTime[i];
-    
+    for (j = 0; j < resource; j++)
+    {
+        for (i = 0; i < process; i++)
+        {
+            total[j] += allocated[i][j];
+        }
     }
 
-    avgwt = avgwt / nprocess;
-    
-
-    // Print average waiting time and average turnaround time
-    printf("\nAverage waiting time: %.2f\n", (float)avgwt);
-    
-    
-}
-void RR() {
-    // Code for Round Robin scheduling
-}
-
-void PriorityScheduling() {
-    // Code for Priority Scheduling
-}
-
-int main() {
-    int choice;
-
-    // Display available CPU scheduling algorithms to the user
-    printf("Choose a CPU scheduling algorithm:\n"
-           "1- FCFS\n"
-           "2- SJF\n"
-           "3- Round Robin\n"
-           "4- Priority Scheduling\n"
-           "Enter your choice (1-4): ");
-    scanf("%d", &choice);
-
-    
-    switch(choice) {
-        case 1:
-            FCFS();
-            break;
-        case 2:
-            SJF();
-            break;
-        case 3:
-            RR();
-            break;
-        case 4:
-            PriorityScheduling();
-            break;
-        default:
-            printf("Invalid choice!\n");
-            break;
+    for(i=0;i<resource;i++)
+    {
+        available[i]=max_instance[j]-total[i];
     }
 
-    return 0;
+    printf("\n need = max - allocated \n");
+    printf("\nNeed Matrix");
+    for (i = 0; i < process; i++)
+    {
+        printf("\n p[%d] ", i);
+        for (j = 0; j < resource; j++)
+        {
+            printf("\t %d", need[i][j]);
+        }
+    }
+    printf("\n------------------------------------------------------------------------------\n");
+    int countsys = 0;
+    while (count != process)
+    {
+        count = temp;
+        for (i = 0; i < process; i++)
+        {
+            for (j = 0; j < resource; j++)
+            {
+                if (need[i][j] <= available[j])
+                {
+                    k++;
+                }
+            }
+            if (k == resource && completed[i] == 0)
+            {
+                system[countsys++] = i;
+
+                completed[i] = 1;
+                for (j = 0; j < resource; j++)
+                {
+                    available[j] = available[j] + allocated[i][j];
+                }
+
+                printf("Work Vector : ");
+                for (j = 0; j < resource; j++)
+                {
+                    printf("%d ", available[j]);
+                }
+                printf("\n");
+
+                count++;
+            }
+            k = 0;
+        }
+
+        if (count == temp)
+        {
+            break;
+        }
+    }
+    printf("\nSafe Sequence: ");
+    for (i = 0; i < process; i++)
+    {
+        printf("P[%d] -> ", system[i]);
+    }
+    for (i = 0; i < process; i++)
+    {
+        if (completed[i] != 1)
+        {
+            printf("\n\n P[%d] not able to allocate", i);
+        }
+    }
+    printf("\n");
 }
